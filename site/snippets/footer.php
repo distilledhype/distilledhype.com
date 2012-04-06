f5666e1f0b3af4e421a0b94b0ecc7d1f1bfce5b7
kahlil <kahlil@kahlil.co>
2012-04-06 22:04:40 +0200

    Cleaned up a little

--- site/snippets/footer.php
+++ site/snippets/footer.php
@@ -5,19 +5,6 @@
     ================================================== -->
     <!-- Placed at the end of the document so the pages load faster -->
     <?php echo js('assets/js/jquery.js') ?>
-    <?php echo js('assets/js/jquery.slabtext.js') ?>
-    <?php echo js('assets/js/bootstrap-transition.js') ?>
-    <?php echo js('assets/js/bootstrap-alert.js') ?>
-    <?php echo js('assets/js/bootstrap-modal.js') ?>
-    <?php echo js('assets/js/bootstrap-dropdown.js') ?>
-    <?php echo js('assets/js/bootstrap-scrollspy.js') ?>
-    <?php echo js('assets/js/bootstrap-tab.js') ?>
-    <?php echo js('assets/js/bootstrap-tooltip.js') ?>
-    <?php echo js('assets/js/bootstrap-popover.js') ?>
-    <?php echo js('assets/js/bootstrap-button.js') ?>
-    <?php echo js('assets/js/bootstrap-collapse.js') ?>
-    <?php echo js('assets/js/bootstrap-carousel.js') ?>
-    <?php echo js('assets/js/bootstrap-typeahead.js') ?>
     <?php echo js('assets/js/script.js') ?>
 
 </body>
--- site/templates/blog.php
+++ site/templates/blog.php
@@ -4,9 +4,6 @@
 
 <section class="container blog">
 
-  <!-- <h1><?php echo html($page->title()) ?></h1>
-  <?php echo kirbytext($page->text()) ?> -->
-
   <?php foreach($page->children()->visible()->flip() as $article): ?>
 
   <article>
@@ -15,7 +12,7 @@
         <?php echo html($article->title()) ?></a></h1>
 
     <p><?php echo excerpt($article->text(), 250) ?></p>
-    <a href="<?php echo $article->url() ?>" class="morelink">Zu den Linkings &rarr;</a>
+    <a href="<?php echo $article->url() ?>" class="morelink">Meahr! &rarr;</a>
   </article>
 
   <?php endforeach ?>
