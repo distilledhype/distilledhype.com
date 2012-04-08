8914e0da0e52a6a0125b28ad6971ddc66d78fd3e
Kahlil Lechelt <kahlil@kahlil.co>
2012-04-08 12:58:01 +0200

    sidebar Änderungen 

--- site/snippets/sidebarcontent.php
+++ site/snippets/sidebarcontent.php
@@ -1,11 +1,14 @@
 <div class="coffeecup"><span>&#9749;</span></div>
 <ul class="vertical-nav">
     <li>
-        <a href="/newsletter" title="Die neueste Ausgabe landet in deinem eBriefkasten bevor die Ausgabe online geht.">
+        <a href="/newsletter" title="Die neueste Ausgabe landet in deiner Mailbox bevor sie online geht.">
             Newsletter
         </a>
     </li>
     <li>
-        <a href="http://twitter.com/dieillstenlinks" title="Schick uns Linktipps!">@dieillstenlinks</a>
+        <a href="http://twitter.com/dieillstenlinks" title="Bleib auf dem Laufenden, sprich mit uns und schick uns Linktipps!">@dieillstenlinks</a>
+    </li>
+    <li>
+        <a href="mailto:hallo@dieillstenlinksvonneulich.de" title="Schick uns Feedback oder Linktipps.">Kontakt</a>
     </li>
 </ul>
