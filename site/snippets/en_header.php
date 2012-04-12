311a8ca261f3ba06d0e35eb08004deec1aed3b9e
Kahlil Lechelt <kahlil@kahlil.co>
2012-04-12 10:38:18 +0200

    added viewport

--- site/snippets/en_header.php
+++ site/snippets/en_header.php
@@ -1,10 +1,11 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-
-  <title>Distilled From The Hype - <?php echo html($page->title()) ?></title>
   <meta charset="utf-8" />
-  <meta name="description" content="<?php echo html($site->description()) ?>" />
+  <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+  <title>Distilled From The Hype - <?php echo html($page->title()) ?></title>
+  <meta name="description" content="Hyperlinks for front-end developers and geeks distilled from the latest hype" />
+  <meta name="viewport" content="width=device-width">
   <meta name="keywords" content="<?php echo html($site->keywords()) ?>" />
   <meta name="robots" content="index, follow" />
 
@@ -12,8 +13,9 @@
   <link rel="icon" href="<?php echo url('assets/img/favicon.png') ?>" type="image/png" />
   <link rel="apple-touch-icon" href="<?php echo u('assets/images/apple-touch-icon.png') ?>" />
 
-  <link href='http://fonts.googleapis.com/css?family=Lato:300,400,700|Open+Sans:400italic,800,700,400,300,' rel='stylesheet' type='text/css'>
+  <meta name="viewport" content="width=device-width">
 
+  <link href='http://fonts.googleapis.com/css?family=Lato:300,400,700|Open+Sans:400italic,800,700,400,300,' rel='stylesheet' type='text/css'>
 
   <?php echo css('assets/css/bootstrap.css') ?>
   <?php echo css('assets/css/responsive.css') ?>
--- site/snippets/header.php
+++ site/snippets/header.php
@@ -1,10 +1,17 @@
-<!DOCTYPE html>
-<html lang="de">
+<!doctype html>
+<!-- paulirish.com/2008/conditional-stylesheets-vs-css-hacks-answer-neither/ -->
+<!--[if lt IE 7]> <html class="no-js lt-ie9 lt-ie8 lt-ie7" lang="en"> <![endif]-->
+<!--[if IE 7]>    <html class="no-js lt-ie9 lt-ie8" lang="en"> <![endif]-->
+<!--[if IE 8]>    <html class="no-js lt-ie9" lang="en"> <![endif]-->
+<!-- Consider adding a manifest.appcache: h5bp.com/d/Offline -->
+<!--[if gt IE 8]><!--> <html class="no-js" lang="de"> <!--<![endif]-->
 <head>
-
-  <title><?php echo html($site->title()) ?> - <?php echo html($page->title()) ?></title>
   <meta charset="utf-8" />
+  <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+  <title><?php echo html($site->title()) ?> - <?php echo html($page->title()) ?></title>
+
   <meta name="description" content="<?php echo html($site->description()) ?>" />
+  <meta name="viewport" content="width=device-width">
   <meta name="keywords" content="<?php echo html($site->keywords()) ?>" />
   <meta name="robots" content="index, follow" />
 
@@ -12,14 +19,14 @@
   <link rel="icon" href="<?php echo url('assets/img/favicon.png') ?>" type="image/png" />
   <link rel="apple-touch-icon" href="<?php echo u('assets/images/apple-touch-icon.png') ?>" />
 
-  <link href='http://fonts.googleapis.com/css?family=Lato:300,400,700|Open+Sans:400italic,800,700,400,300,' rel='stylesheet' type='text/css'>
+  <meta name="viewport" content="width=device-width">
 
+  <link href='http://fonts.googleapis.com/css?family=Lato:300,400,700|Open+Sans:400italic,800,700,400,300,' rel='stylesheet' type='text/css'>
 
   <?php echo css('assets/css/bootstrap.css') ?>
   <?php echo css('assets/css/responsive.css') ?>
 
 </head>
-
 <body>
 	<header class="container header">
     	<h1><a href="<?php echo url() ?>" id="header">Die illsten Links von neulich</a></h1>
