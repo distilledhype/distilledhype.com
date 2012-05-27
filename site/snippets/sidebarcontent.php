d4add3558b29a7632e53bde7a9b0281fbc0e0411
Kahlil Lechelt <kahlil@kahlil.co>
2012-05-27 10:59:59 +0200

    fix

--- site/snippets/sidebarcontent.php
+++ site/snippets/sidebarcontent.php
@@ -9,7 +9,7 @@
         <a href="http://twitter.com/distilledhype" title="Stay up to date, send us link tips and holler.">@distilledhype</a>
     </li>
     <li>
-        <a href="/newsletter" title="Get the latest issue in your inbox before it's posted online.">RSS</a>
+        <a href="/newsletter" title="Get the latest issue in your inbox before it's posted online.">Newsletter</a>
     </li>
     <li>
         <a href="mailto:hi@distilledfromthehype.com" title="Send us feedback or link tips">Contact</a>
