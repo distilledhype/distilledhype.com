cd3e406a3897e7211f52daccf5ef8cb9696ef40a
Kahlil Lechelt <kahlil@kahlil.co>
2012-06-14 12:28:28 +0200

    Prevent CSS transitions on load

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,5 @@
 $('.blogarticle').find('h3').not(':first').before('<div class="coffee">&#9749;</div>');
 
-$(document).scroll(function() {
-	console.log('what');
-	$(this).find('#logo').addClass('shadowed');
-	if($(document).scrollTop() === 0) {
-		$(this)
-			.find('#logo')
-			.removeClass('shadowed');
-	}
+$(window).load(function() {
+    $("body").removeClass("preload");
 });
