bfdb196c16fca0bf6eb9dece0300d58b6b9eaef5
Kahlil Lechelt <kahlil@kahlil.co>
2012-08-24 21:20:48 +0200

    Sidebar changes.

--- site/snippets/sidebarcontent.php
+++ site/snippets/sidebarcontent.php
@@ -2,15 +2,18 @@
 <div class="coffeecup"><img src="/assets/img/coffeecup.png" alt="Coffee cup"></div>
 <ul class="vertical-nav">
     <li>
-        <a href="http://twitter.com/distilledhype" title="Stay up to date, send us link tips and holler.">@distilledhype</a>
+        <a href="http://twitter.com/distilledhype" title="Stay up to date, send link tips and holler.">@distilledhype</a>
     </li>
     <li>
-        <a href="/newsletter" title="Get the latest issue in your inbox before it's posted online.">Newsletter</a>
+        <a href="/newsletter" title="Get a newsletter. Maybe. Sometimes.">Newsletter</a>
     </li>
     <li>
-        <a href="mailto:hi@distilledfromthehype.com" title="Send us feedback or link tips">Contact</a>
+        <a href="mailto:hi@distilledhype.com" title="Send feedback or link tips.">Contact</a>
     </li>
     <li>
         <a href="http://feeds.feedburner.com/DistilledFromTheHype" title="Get the feed.">RSS</a>
+    </li>
+    <li>
+        <a href="https://www.facebook.com/distilledhype" title="Like it up.">f</a>
     </li>
 </ul>
