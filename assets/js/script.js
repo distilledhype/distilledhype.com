82cb6a677d7e9c198fabb6e93c03470be145b74f
Kahlil Lechelt <kahlil@kahlil.co>
2012-09-09 19:40:16 +0200

    new post

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,25 @@
 $('.blogarticle').find('h3').not(':first').before('<div class="coffee">&#9749;</div>');
 
 $(window).load(function() {
+
+    var $morelink = $('.morelink');
+
     $('body').removeClass( "preload" );
-    $('.morelink').find( 'img' ).on({
+    $morelink.find( 'img' ).on({
         mouseenter: function() {
-           $(this).parent().next( 'time' ).find( 'a' ).css( 'color', '#555' );
+           $(this).parent().next( 'time' ).fadeIn();
         },
         mouseout: function() {
-            $(this).parent().next( 'time' ).find( 'a' ).removeAttr( 'style' );
+            $(this).parent().next( 'time' ).fadeOut();
+        }
+    });
+
+    $morelink.on({
+        focus: function() {
+           $(this).next( 'time' ).fadeIn();
+        },
+        focusout: function() {
+            $(this).next( 'time' ).fadeOut();
         }
     });
 });
