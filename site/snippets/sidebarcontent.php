17e47fae4ad969e1050e7606154c55898184b9c3
Kahlil Lechelt <kahlil@kahlil.co>
2012-09-13 21:00:48 +0200

    responsive corrections

--- site/snippets/sidebarcontent.php
+++ site/snippets/sidebarcontent.php
@@ -1,19 +1,21 @@
 <!-- <div class="coffeecup"><span>&#9749;</span></div> -->
-<div class="coffeecup"><img src="/assets/img/coffeecup.png" alt="Coffee cup" width="160" height="160"></div>
-<ul class="vertical-nav">
-    <li>
-        <a href="http://twitter.com/distilledhype" title="Stay up to date, send link tips and holler.">@distilledhype</a>
-    </li>
-    <li>
-        <a href="/newsletter" title="Get a newsletter. Maybe. Sometimes.">Newsletter</a>
-    </li>
-    <li>
-        <a href="mailto:hi@distilledhype.com" title="Send feedback or link tips.">Contact</a>
-    </li>
-    <li>
-        <a href="http://feeds.feedburner.com/DistilledFromTheHype" title="Get the feed.">RSS</a>
-    </li>
-    <li>
-        <a href="https://www.facebook.com/distilledhype" title="Like it up.">f</a>
-    </li>
-</ul>
+<div class="clearfix">
+    <div class="coffeecup"><img src="/assets/img/coffeecup.png" alt="Coffee cup" width="160" height="160"></div>
+    <ul class="vertical-nav">
+        <li>
+            <a href="http://twitter.com/distilledhype" title="Stay up to date, send link tips and holler.">@distilledhype</a>
+        </li>
+        <li>
+            <a href="/newsletter" title="Get a newsletter. Maybe. Sometimes.">Newsletter</a>
+        </li>
+        <li>
+            <a href="mailto:hi@distilledhype.com" title="Send feedback or link tips.">Contact</a>
+        </li>
+        <li>
+            <a href="http://feeds.feedburner.com/DistilledFromTheHype" title="Get the feed.">RSS</a>
+        </li>
+        <li>
+            <a href="https://www.facebook.com/distilledhype" title="Like it up.">f</a>
+        </li>
+    </ul>
+</div>
