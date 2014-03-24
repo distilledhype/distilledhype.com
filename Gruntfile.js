df83ebda5b4d94748b5d21d5619696692a417346
Kahlil Lechelt <hello@kahlil.co>
2014-03-24 13:46:05 +0100

    Edit Gruntfile

--- Gruntfile.js
+++ Gruntfile.js
@@ -5,6 +5,8 @@ module.exports = function(grunt) {
 
   // Default task.
   grunt.registerTask('default', ['jshint']);
+  // Scaffold link posts, text posts and grunt tasks.
   grunt.registerTask('lp', ['textfile:linkpost']);
   grunt.registerTask('tp', ['textfile:textpost']);
+  grunt.registerTask('gt', ['textfile:grunttask']);
 };
