83bd1f4da7fd4764d40f35a82691867f5c2ffa3d
Kahlil Lechelt <hello@kahlil.co>
2014-04-25 20:00:15 +0200

    fix

--- site/snippets/masthead.php
+++ site/snippets/masthead.php
@@ -1,6 +1,5 @@
 <div class="masthead">
   <h3 class="masthead-title">
-    <a href="<?php echo url() ?>" id="header">Distilled Hype</a> <small>by <a href="/about">Kahlil Lechelt</a></small>
+    <a href="<?php echo url() ?>" id="header">Distilled Hype</a> <small>by <a href="/about" style="text-decoration:underline">Kahlil Lechelt</a></small>
   </h3>
-  <?php snippet('menu') ?>
 </div>
